9c32d4a43ea4fe285f13f47f7bddd10ab3ccd38e
liaocj <liaocj@yusur.tech>
2024-01-02 01:15:18 +0800

    fix err

--- src/dpdk/drivers/net/virtio/virtio_rxtx.c
+++ src/dpdk/drivers/net/virtio/virtio_rxtx.c
@@ -185,8 +185,11 @@ virtqueue_dequeue_burst_rx(struct virtqueue *vq, struct rte_mbuf **rx_pkts,
 		cookie = (struct rte_mbuf *)vq->vq_descx[desc_idx].cookie;
 		rx_times[vq->vq_queue_index] ++;
 		if (unlikely(cookie == NULL)) {
-			PMD_DRV_LOG(ERR, "last_used_index:0x%x, avail_idx:0x%x, current_hw_used.idx:0x%x vq_free_cnt:%x, rx_times:%u",
-				vq->vq_used_cons_idx, vq->vq_split.ring.avail->idx, vq->vq_split.ring.used->idx, vq->vq_free_cnt, rx_times[vq->vq_queue_index]);
+			PMD_DRV_LOG(ERR, "last_used_index:0x%x, avail_idx:0x%x, current_hw_used.idx:0x%x(elem_id:0x%x, elem_len:0x%x), vq_free_cnt:%x, rx_times:%u",
+				vq->vq_used_cons_idx, vq->vq_split.ring.avail->idx, vq->vq_split.ring.used->idx,
+				vq->vq_split.ring.used->ring[vq->vq_split.ring.used->idx].id,
+				vq->vq_split.ring.used->ring[vq->vq_split.ring.used->idx].len,
+				 vq->vq_free_cnt, rx_times[vq->vq_queue_index]);
 			uint16_t start_idx = ((uint16_t)(used_idx - 8)) % vq->vq_nentries;
 			uint16_t end_idx =  ((uint16_t)(used_idx + 8)) % vq->vq_nentries;
 			if (end_idx < start_idx) {
